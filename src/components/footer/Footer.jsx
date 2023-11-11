b55acbdc39e776dfba7444f30772802e932e455e
alameenshareef <ameendevhouse@gmail.com>
2023-11-11 22:08:14 +0530

    first commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,54 @@
+import "./footer.css"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+<div className="fLists">
+    <ul className="fList">
+        <li className="listitem">Countries</li>
+        <li className="listitem">Regions</li>
+        <li className="listitem">Cities</li>
+        <li className="listitem">Districts</li>
+        <li className="listitem">Airports</li>
+        <li className="listitem">Hotels</li>
+    </ul>
+    <ul className="fList">
+        <li className="listitem">Countries</li>
+        <li className="listitem">Regions</li>
+        <li className="listitem">Cities</li>
+        <li className="listitem">Districts</li>
+        <li className="listitem">Airports</li>
+        <li className="listitem">Hotels</li>
+    </ul>
+    <ul className="fList">
+        <li className="listitem">Countries</li>
+        <li className="listitem">Regions</li>
+        <li className="listitem">Cities</li>
+        <li className="listitem">Districts</li>
+        <li className="listitem">Airports</li>
+        <li className="listitem">Hotels</li>
+    </ul>
+    <ul className="fList">
+        <li className="listitem">Countries</li>
+        <li className="listitem">Regions</li>
+        <li className="listitem">Cities</li>
+        <li className="listitem">Districts</li>
+        <li className="listitem">Airports</li>
+        <li className="listitem">Hotels</li>
+    </ul>
+    <ul className="fList">
+        <li className="listitem">Countries</li>
+        <li className="listitem">Regions</li>
+        <li className="listitem">Cities</li>
+        <li className="listitem">Districts</li>
+        <li className="listitem">Airports</li>
+        <li className="listitem">Hotels</li>
+    </ul>
+    </div> 
+    <div className="fText">Copyright @ 2022 Lamabookng</div>
+
+    </div>
+  )
+}
+
+export default Footer
